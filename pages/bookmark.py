7f6501888e46fab7831818528d2f433f74b37428
Lucas Thichaki <lucas.thichaki@unesp.br>
2024-11-29 10:12:50 -0300

    update bookmark

--- pages/bookmark.py
+++ pages/bookmark.py
@@ -15,15 +15,10 @@ st.balloons()
 conn = mysql.connector.connect(host=st.secrets.DB_HOST, user=st.secrets.DB_USERNAME, password=st.secrets.DB_PASSWORD,port = st.secrets.DB_PORT, db=st.secrets.DB_NAME, auth_plugin='mysql_native_password')
 cursor = conn.cursor()
 
-@st.cache_data
-def run_query(query):
-    cursor.execute(query)
-    res = cursor.fetchall()
-    df = pd.DataFrame(res, columns=cursor.column_names)
-    return df
-
 inp = f"SELECT NO_ENTIDADE FROM escola CROSS JOIN (SELECT ID_ESC FROM bookmark WHERE ID_USU = {st.session_state.user_state['ID']}) as user_bookmark WHERE CO_ENTIDADE = ID_ESC"
-df = run_query(inp)
+cursor.execute(inp)
+res = cursor.fetchall()
+df = pd.DataFrame(res, columns=cursor.column_names)
 if len(df) != 0:
     st.write(df)
 else:
